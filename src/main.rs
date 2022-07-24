de0909a55771af543ef0603d08c7b5f31dae8726
factal <joji0425jp@gmail.com>
2022-07-25 02:07:32 +0900

    add: dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(core_intrinsics)]
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &PanicINfo) -> ! {
+    intrinsics::abort();
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let framebuffer = 0xb8000 as *mut u8;
+
+    unsafe {
+        framebuffer
+        .offset(1)
+        .write_volatile(0x30);
+    }
+
+    loop {}
 }
